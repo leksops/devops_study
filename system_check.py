786ec1105a9166116c88aae5f946976ab6fe1c2d
Sasha <sashamanatskova@mail.ru>
2026-08-25 21:59:17 +0300

    fix script

--- system_check.py
+++ system_check.py
@@ -1,7 +1,7 @@
 network_statuses = ["🟢 10.0.0.1", "🔴 10.0.0.2", "🟢 10.0.0.3", "🔴 10.0.0.4"]
 broken_servers = []
 
-for status in network_statuses :
+for status in network_statuses:
     if "🔴" in status:
         broken_servers.append(status)
 
